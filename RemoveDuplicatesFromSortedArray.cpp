f11ba6276c75be0a052fe4e5022309f3f953edb1
echoht <553052687@qq.com>
2019-04-06 22:41:54 +0800

    add new algorithms

---
+++ RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,9 @@
+//
+//  RemoveDuplicatesFromSortedArray.cpp
+//  LeetcodeByEcho
+//
+//  Created by Dylan326 on 2019/4/5.
+//  Copyright © 2019 DE. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ RemoveElement_27.cpp
@@ -0,0 +1,9 @@
+//
+//  RemoveElement_27.cpp
+//  LeetcodeByEcho
+//
+//  Created by Dylan326 on 2019/4/6.
+//  Copyright © 2019 DE. All rights reserved.
+//
+
+#include <stdio.h>
